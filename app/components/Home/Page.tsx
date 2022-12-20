4402cd7e0cd9761823a374fd0a5aac5d9ccbee81
yudai2929 <yudai201729@icloud.com>
2022-12-20 23:24:00 +0900

    👍feat:pathpidaを追加

--- app/components/Home/Page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useSignInWithGoogle } from "../../hooks/auth/useSignIn";
-import LoginButton from "../ui/LoginButton";
-
-export const Page = (): JSX.Element => {
-  const { mutate: login } = useSignInWithGoogle();
-  return <LoginButton onClick={login} />;
-};
