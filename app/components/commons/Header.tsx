ecbe4b47fd3d5f9eed6c469b37204c6701043cbe
yudai2929 <yudai201729@icloud.com>
2022-12-21 16:38:17 +0900

    💄feat:ゲーム選択画面のヘッダーを作成

---
+++ app/components/commons/Header.tsx
@@ -0,0 +1,47 @@
+import { Avatar, Button, Dropdown, Navbar, Text } from "@nextui-org/react";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <Navbar>
+      <Navbar.Brand>
+        <Text b color="inherit" hideIn="xs" css={{ fontSize: "$2xl" }}>
+          BUX
+        </Text>
+      </Navbar.Brand>
+
+      <Navbar.Content>
+        <Dropdown placement="bottom-right">
+          <Navbar.Item>
+            <Dropdown.Trigger>
+              <Avatar
+                bordered
+                as="button"
+                color="secondary"
+                size="md"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </Dropdown.Trigger>
+          </Navbar.Item>
+          <Dropdown.Menu
+            aria-label="User menu actions"
+            color="secondary"
+            onAction={(actionKey) => console.log({ actionKey })}
+          >
+            <Dropdown.Item key="profile" css={{ height: "$18" }}>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                Signed in as
+              </Text>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                zoey@example.com
+              </Text>
+            </Dropdown.Item>
+            <Dropdown.Item key="logout" withDivider color="error">
+              Log Out
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar.Content>
+    </Navbar>
+  );
+};
