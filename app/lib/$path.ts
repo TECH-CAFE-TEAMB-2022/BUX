f9f6365dd8248c250cc8500079c75b6eec90c6d1
mitsuYashi <hm20011027@gmail.com>
2022-12-21 16:39:20 +0900

    feat: top nav routing

--- app/lib/$path.ts
+++ app/lib/$path.ts
@@ -1,12 +1,11 @@
 export const pagesPath = {
   "stages": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/stages' as const, hash: url?.hash }),
     _level: (level: string | number) => ({
       _id: (id: string | number) => ({
         $url: (url?: { hash?: string }) => ({ pathname: '/stages/[level]/[id]' as const, query: { level, id }, hash: url?.hash })
-      })
-    }),
-    $url: (url?: { hash?: string }) => ({ pathname: '/stages' as const, hash: url?.hash })
+      }),
+      $url: (url?: { hash?: string }) => ({ pathname: '/stages/[level]' as const, query: { level }, hash: url?.hash })
+    })
   },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
---
+++ app/pages/stages/[level]/index.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import TopNav from "../../../components/TopNav";
+import { useRouter } from "next/router";
+
+const level = () => {
+  return <TopNav></TopNav>;
+};
+
+export default level;
